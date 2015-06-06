0f8c76cdda85a64dacf43b2782295a8bc50b8076
sara89sgm <sara89sgm@gmail.com>
2015-06-06 16:42:03 +0100

    adding file

--- web/scripts/audioRecord.js
+++ web/scripts/audioRecord.js
@@ -84,7 +84,17 @@ define(['jquery'], function($) {
 			var audiogramObject = new AudiogramItem();
 			audiogramObject.set("username", username);
 			audiogramObject.set("audioURL", audio);
-			audiogramObject.save();
+			audiogramObject.save(null, {
+				  success: function(gameScore) {
+				    // Execute any logic that should take place after the object is saved.
+				    alert('New object created with objectId: ' + gameScore.id);
+				  },
+				  error: function(gameScore, error) {
+				    // Execute any logic that should take place if the save fails.
+				    // error is a Parse.Error with an error code and message.
+				    alert('Failed to create new object, with error code: ' + error.message);
+				  }
+				});
 
 			
 		},
