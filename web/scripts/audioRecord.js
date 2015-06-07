d4c6ea6847bdad036aaf42423366d3c7993f57fe
sara89sgm <sara89sgm@gmail.com>
2015-06-07 09:39:50 +0100

    adding play button

--- web/scripts/audioRecord.js
+++ web/scripts/audioRecord.js
@@ -54,7 +54,7 @@ define(['jquery'], function($) {
 
 		bindPlays : function(){
 			$('.play-button-hover').on('click', function(e){
-				console.log(e.currentTarget);
+				console.log(e);
 				var au = e.currentTarget.parentElement;
 				console.log(au);
 				if(!au.paused){
