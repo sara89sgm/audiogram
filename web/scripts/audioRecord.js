939774bf0250ebd14c085906b304585dfe43453a
sara89sgm <sara89sgm@gmail.com>
2015-06-07 09:02:33 +0100

    adding play button

--- web/scripts/audioRecord.js
+++ web/scripts/audioRecord.js
@@ -43,12 +43,25 @@ define(['jquery'], function($) {
 				filter = e.target.dataset.filter;
 				
 				window.location.hash = '#record';
-			})
+			});
+
+			priv.bindPlays();
 
 
 			tuna = new Tuna(context);
 
 		},
+
+		bindPlays : function(){
+			$('.audio-players').on('click', function(e)){
+				if(!e.target.firstChild.paused){
+					e.target.firstChild.stop();
+				}else{
+					e.target.firstChild.play();
+				}
+				
+			}
+		}
 
 		record: function(stream){
 			  navigator.getUserMedia({audio: true}, function(localMediaStream){
@@ -136,10 +149,11 @@ define(['jquery'], function($) {
 		},
 
 		addItem : function(audio, username){
-			var html= '<li class="ui-li-has-thumb ui-first-child hidden"><a href="#" class="ui-btn ui-btn-icon-right ui-icon-caret-r"><audio id="player" src="'+audio+'"></audio><h2>'+username+'</h2><p>'+username+'</p></a></li>';
+			var html= '<li class="ui-li-has-thumb ui-first-child hidden"><a href="#" class="ui-btn ui-btn-icon-right ui-icon-caret-r"><audio id="player" src="'+audio+'"></audio><h2>'+username+'</h2><p>'+username+'</p></a><div class="play-button-hover"><img src="../img/play-button.png" /> </div></li>';
 
 			$('.audio-list').prepend(html);
 			$('li.hidden').delay('400').slideToggle('400');
+			priv.bindPlays();
 		},
 
 
